f7ae179db63a44c94c3fb5d2c3e22e7de80a2673
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:27:45 +0200

    ready to handle event

--- cobalt.snackbar.js
+++ cobalt.snackbar.js
@@ -1,30 +1,41 @@
-(function (cobalt) {
-    var plugin = {
-        classes: {
-			ios: 'CobaltSnackbarPlugin',
-			android: 'io.kristal.snackbarplugin.SnackbarPlugin'
-        },
-        duration: {
-            INFINITE: -2,
-            SHORT: -1,
-            LONG: 0
-        },
-        init: function () {
-            cobalt.snackbar = {
-                show: this.show.bind(this),
-                duration: this.duration
-            };
-        },
-        show: function (options) {
-            if (options) {
-                if (typeof options === 'string') {
-                    cobalt.plugins.send(this, 'show', { text: options }, options && options.callback);
-                }
-                else {
-                    cobalt.plugins.send(this, 'show', options, options && options.callback);
-                }
-            }
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: 'CobaltSnackbarPlugin',
+      android: 'io.kristal.snackbarplugin.SnackbarPlugin'
+    },
+    duration: {
+      INFINITE: -2,
+      SHORT: -1,
+      LONG: 0
+    },
+    init: function() {
+      cobalt.snackbar = {
+        show: this.show.bind(this),
+        duration: this.duration
+      };
+    },
+    show: function(options) {
+      if (options) {
+        if (typeof options === 'string') {
+          cobalt.plugins.send(this, 'show', {text: options}, options && options.callback);
         }
-    };
-    cobalt.plugins.register(plugin);
+        else {
+          this.onSnackbarResult = options.callback;
+          cobalt.plugins.send(this, 'show', {
+            text: options.text,
+            duration: options.duration,
+            button: options.button,
+            buttonColor: options.buttonColor
+          });
+        }
+      }
+    },
+    handleEvent: function(json) {
+      if (typeof this.onSnackbarResult === 'function') {
+        this.onSnackbarResult(json.data)
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
